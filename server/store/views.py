2b85352329c5c04e6a1682470273741fb4505094
dmta9796 <dmta9796@colorado.edu>
2019-06-28 00:46:32 -0600

    finally got data to client

--- server/store/views.py
+++ server/store/views.py
@@ -1,13 +1,20 @@
 from django.http import HttpResponse
+from django.http import JsonResponse
 # import pymysql
 from . import models
 from . import seedscript
 
-def index(request):
-    data = sendtodb()
-    return HttpResponse(data)
 
+def index(request):
+    return HttpResponse('at the index')
 
 def sendtodb():
     data = seedscript.data()
     return data
+
+def data(request):
+    data = sendtodb()
+    #response = HttpResponse(data)
+    response = HttpResponse(data, content_type='text/plain')
+    #print(response)
+    return response
