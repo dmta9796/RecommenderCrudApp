ce1bacc4622c022b08e06190fb72a5fc59712aa8
dmta9796 <dmta9796@colorado.edu>
2019-07-30 15:57:14 -0600

    code cleanup and improvement of file parser

--- server/store/seedscript.py
+++ server/store/seedscript.py
@@ -3,24 +3,6 @@ import os
 import re
 from . import models
 
-def processrow(row):
-        result = []
-        result.append(row[0])
-        index = row[1].find('-')
-        item = row[1][0: index]
-        result.append(item)
-        idxDetails = row[1].find('Details:')
-        idxFabric = row[1].find('Fabric:')
-        idxWeight = row[1].find('Weight:')
-        details = row[1][idxDetails:idxFabric]
-        fabric = row[1][idxFabric:idxWeight] #.split('.')[0:2]
-        #strfabric = fabric[0]+'.'+fabric[1]
-        #print(fabric)
-        weight = row[1][idxWeight:]
-        result.append(fabric)
-        result.append(weight)
-        return result
-
 def processdata(data):
         result = []
         result.append(data[0]) # id number of item
@@ -29,8 +11,10 @@ def processdata(data):
         info[0] = info[0].split('-')[0]
         info[1] = info[1].split('</li>')
         info[2] = info[2].split(':')
+        del info[2][0]
+        info[2][0] = info[2][0].replace('</b>','')
         if(len(info) == 5):
-                info[3] = info[3].split('</b>')[1]
+                info[3] = int(info[3].split('</b>')[1].split('g')[0].replace('(','')) #weight of product in grams
         del info[1]
         print(info,len(info))
 
@@ -43,7 +27,6 @@ def data():
                 reader = csv.reader(f)
                 for row in reader:
                         row = processdata(row)
-                        #row = processrow(row)
                         data.append(row)
         return data   
  
